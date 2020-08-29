99dc578ea3287f932f51ae6462ffc06aab6a74c1
baalazamon <bartosz.lipinski@outlook.com>
2020-08-28 20:56:47 -0500

    add rinkeby

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "bf6d6ca0b05a41b194e094613b85a40b";
+//
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+    },
   }
 };
